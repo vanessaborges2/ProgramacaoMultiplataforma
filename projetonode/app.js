1c6b07df65973a836e8cd2cf6aacc6176954e40b
vanessaborges2 <vanessa.borges2@fatec.sp.gov.br>
2024-03-14 17:21:15 -0300

    Definição

--- projetonode/app.js
+++ projetonode/app.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
-const urlEncodedParser = require('body-parser');
 const express = require('express');
+const urlEncodedParser = express.json();
 const app = express()
 
 app.listen(8080, () => {
@@ -16,3 +16,30 @@ app.get("/", (req, res)=> {
     });
 });
 
+app.post("/acessar", urlEncodedParser, (req, res)=>{
+    res.redirect("/principal");
+});
+
+app.get("/principal", (req,res)=>{
+    fs.readFile("src/principal.html", (e, dados)=>{
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(dados);
+        res.end();
+    });
+});
+
+app.get("/livros", (req, res)=>{
+    fs.readFile("src/livros.html", (e, dados)=>{
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(dados);
+        res.end();
+    });
+});
+
+app.get("/livros/novo", (req, res)=>{
+    fs.readFile("src/novo_livro.html", (e, dados)=>{
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.write(dados);
+        res.end();
+    });
+});
